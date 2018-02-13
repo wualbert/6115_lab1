506cf51e7e7e36359e79742d1752fc2d5974185a
student <student>
2018-02-12 21:09:53 -0500

    completed equivalent program

--- Exercise1/exercise1_equivalent.asm
+++ Exercise1/exercise1_equivalent.asm
@@ -1,7 +1,8 @@
 ;6.115 Lab 1 Exercise 1 Functionally Equivalent Program
 ;Albert Wu
-
-clr 90h    ;Clears P1
+clr A            ;Clears accumulator byte (special command)
+mov P1, A        ;Moves value in A to P1
+setb P1          ;Set P1 as 1
 loop:
     sjmp loop
     
